0dea905f910cdf54c12782175930373f0fb56388
TimVanDorpe <tim.van.dorpe@hotmail.com>
2016-02-10 18:19:27 +0100

    #1 Aanmaken project via Github

---
+++ Groep9.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Groep9.NET.MvcApplication" Language="C#" %>
---
+++ Groep9.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Groep9.NET.Startup))]
+namespace Groep9.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
