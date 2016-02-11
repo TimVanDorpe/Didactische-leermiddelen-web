0caa1224c9d8f72d3dc7183df5097e4f81f4e780
Jens De Dene <jens.dedene.t9216@student.hogent.be>
2016-02-11 13:33:11 +0100

    #2 UC1 inloggen domeinModel aangemaakt

---
+++ Groep9.NET/Models/Domein/Gebruikers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace Groep9.NET {
+    public interface Gebruikers {
+        string Naam { get; set; }
+        string Wachtwoord { get; set; }
+        int GebruikersID { get; }
+
+        void LogIn();
+    }
+}
---
+++ Groep9.NET/Models/Domein/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace Groep9.NET {
+    public class Student : Gebruikers {
+        public int GebruikersID {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Naam {
+            get {
+                throw new NotImplementedException();
+            }
+
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Wachtwoord {
+            get {
+                throw new NotImplementedException();
+            }
+
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void LogIn() {
+            throw new NotImplementedException();
+        }
+    }
+}
