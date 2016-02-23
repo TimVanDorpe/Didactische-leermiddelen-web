71015321181e4dba9644b1374395b0aa3837b844
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-02-23 13:27:46 +0100

    Merged edits

--- Groep9.NET/Models/DAL/Initializer.cs
+++ Groep9.NET/Models/DAL/Initializer.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
 using System.Data.Entity;
-<<<<<<< Updated upstream
 using Groep9.NET.Models.Domein;
-=======
 using System.Web;
 using Groep9.NET.Models.Domein;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
->>>>>>> Stashed changes
 
 namespace Groep9.NET.Models.DAL
 {
@@ -21,43 +18,29 @@ namespace Groep9.NET.Models.DAL
           
             try
             {
+                Doelgroep kleuters = new Doelgroep { Naam = "Kleuters" };
+                Doelgroep lagereSchool = new Doelgroep { Naam = "Lagere School" };//manier 1
 
-<<<<<<< Updated upstream
-                Doelgroep Kleuters = new Doelgroep { Naam = "Kleuters" };
-                Doelgroep LagereSchool = new Doelgroep { Naam = "Lagere School" };//manier 1
+                Leergebied kaarten = new Leergebied("Kaartprojectie");//manier 2
+                Leergebied kansen = new Leergebied("Kansberekening");
+                Leergebied tellen = new Leergebied("Tellen");
+                Leergebied behendigheid = new Leergebied("Behendigheid"); // 2 vss manieren om dit te proberen doen werken, geen van beide die dus werkt..
 
-                Leergebied Kaarten = new Leergebied("Kaartprojectie");//manier 2
-                Leergebied Kansen = new Leergebied("Kansberekening");
-                Leergebied Tellen = new Leergebied("Tellen");
-                Leergebied Behendigheid = new Leergebied("Behendigheid"); // 2 vss manieren om dit te proberen doen werken, geen van beide die dus werkt..
+                context.Doelgroepen.Add(kleuters);
+                context.Doelgroepen.Add(lagereSchool);
 
-                context.Doelgroepen.Add(Kleuters);
-                context.Doelgroepen.Add(LagereSchool);
-
-                context.Leergebieden.Add(Kaarten);
-                context.Leergebieden.Add(Kansen);
-                context.Leergebieden.Add(Tellen);
-                context.Leergebieden.Add(Behendigheid);
+                context.Leergebieden.Add(kaarten);
+                context.Leergebieden.Add(kansen);
+                context.Leergebieden.Add(tellen);
+                context.Leergebieden.Add(behendigheid);
                 context.SaveChanges();
 
-                Product landkaart = new Product("landkaart.jpg", 4, "Landkaart", "Map van België", 8.55, 10, false, "Aalst", "Hogent", LagereSchool, Kaarten);
-                Product rekenmachine = new Product("rekenmachine.jpg", 5, "Rekenmachine", "Rekenmachine van merk ..", 8.55, 10, false, "Aalst", "Hogent", LagereSchool, Kaarten);
-                Product dobbelsteenschatkist = new Product("dobbelsteen.jpg", 6, "Dobbelsteen schatkist", "koffertje met verschillende soorten dobbelstenen: blanco, met cijfers, ..", 35, 1, true, "GLEDE 1.011", "Hogent", Kleuters, Tellen);
-                Product blancodraaischijf = new Product("blanco_draaischijf.PNG", 7, "Blanco shijf", "Met verschillende blanco shijven in hard papier", 31.45, 1, true, "GLEDE 1.011", "HoGent", Kleuters, Kansen);
-                Product spinners_klass_ass = new Product("Magnspinner.jpg", 8, "Magnetische spinner", "Magnetische spinners in de vorm van een pijl, een vinger en een potlood", 19.2, 1, true, "GLEDE 1.011", "Hogent", Kleuters , Behendigheid);
+                Product landkaart = new Product("landkaart.jpg", 4, "Landkaart", "Map van België", 8.55, 10, false, "Aalst", "Hogent", lagereSchool, kaarten);
+                Product rekenmachine = new Product("rekenmachine.jpg", 5, "Rekenmachine", "Rekenmachine van merk ..", 8.55, 10, false, "Aalst", "Hogent", lagereSchool, kaarten);
+                Product dobbelsteenschatkist = new Product("dobbelsteen.jpg", 6, "Dobbelsteen schatkist", "koffertje met verschillende soorten dobbelstenen: blanco, met cijfers, ..", 35, 1, true, "GLEDE 1.011", "Hogent", kleuters, tellen);
+                Product blancodraaischijf = new Product("blanco_draaischijf.PNG", 7, "Blanco shijf", "Met verschillende blanco shijven in hard papier", 31.45, 1, true, "GLEDE 1.011", "HoGent", kleuters, kansen);
+                Product spinners_klass_ass = new Product("Magnspinner.jpg", 8, "Magnetische spinner", "Magnetische spinners in de vorm van een pijl, een vinger en een potlood", 19.2, 1, true, "GLEDE 1.011", "Hogent", kleuters , behendigheid);
                 
-
-
-=======
-               
-
-                Product landkaart = new Product("landkaart.jpg", 4, "Landkaart", "Map van België", 8.55, 10, false, "Aalst", "Hogent", "Landmeetkunde", "Kaartprojectie");
-                Product rekenmachine = new Product("rekenmachine.jpg", 5, "Rekenmachine", "Rekenmachine van merk ..", 8.55, 10, false, "Aalst", "Hogent", "Landmeetkunde", "Kaartprojectie");
-                Product dobbelsteenschatkist = new Product("dobbelsteen.jpg", 6, "Dobbelsteen schatkist", "koffertje met verschillende soorten dobbelstenen: blanco, met cijfers, ..", 35, 1, true, "GLEDE 1.011", "Hogent", "Kleuters", "Tellen");
-                Product blancodraaischijf = new Product("blanco_draaischijf.PNG", 7, "Blanco shijf", "Met verschillende blanco shijven in hard papier", 31.45, 1, true, "GLEDE 1.011", "HoGent", "Kleuters", "Kansberekening");
-                Product spinners_klass_ass = new Product("Magnspinner.jpg", 8, "Magnetische spinner", "Magnetische spinners in de vorm van een pijl, een vinger en een potlood", 19.2, 1, true, "GLEDE 1.011", "Hogent", "Kleuters", "Behendigheid");
-                
->>>>>>> Stashed changes
                 context.Producten.Add(landkaart);
                 context.Producten.Add(rekenmachine);
                 context.Producten.Add(dobbelsteenschatkist);
