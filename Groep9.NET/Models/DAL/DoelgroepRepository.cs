37ef1e7332555cf88781ec864857af883e908822
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-02-25 13:37:20 +0100

    problemen oplossen

---
+++ Groep9.NET/Models/DAL/DoelgroepRepository.cs
@@ -0,0 +1,35 @@
+﻿using Groep9.NET.Models.Domein;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Groep9.NET.Models.DAL
+{
+    public class DoelgroepRepository : IDoelgroepRepository
+    {
+        private Context context;
+        private DbSet<Doelgroep> Doelgroepen;
+        public DoelgroepRepository(Context context)
+        {
+            this.context = context;
+            Doelgroepen = context.Doelgroepen;
+        }
+
+        public Doelgroep FindById(int id)
+        {
+            return Doelgroepen.Find(id);
+               
+          }
+
+        public IQueryable<Doelgroep> VindAlleDoelgroepen()
+        {
+            return Doelgroepen;
+        }
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Groep9.NET/Models/Domein/IDoelgroepRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Groep9.NET.Models.Domein
+{
+    public interface IDoelgroepRepository
+    {
+        IQueryable<Doelgroep> VindAlleDoelgroepen();
+
+
+        Doelgroep FindById(int id);
+    }
+}
