757eae9de7c8f1e17b6a0b5ac3dabc1156ead16c
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-03-11 17:58:27 +0100

    Knop "toevoegen aan verlanglijst" kan BIJNA veranderen

--- Groep9.NET/Scripts/custom.js
+++ Groep9.NET/Scripts/custom.js
@@ -16,22 +16,39 @@ function mobileViewUpdate() {
 $(window).load(mobileViewUpdate);
 $(window).resize(mobileViewUpdate);
 
-var items = $.map(
-    $(this).sortable('toArray'),
-    function (item, index) {
-        return { "item": item };
-    }
-);
+//var items = $.map(
+//    $(this).sortable('toArray'),
+//    function (item, index) {
+//        return { "item": item };
+//    }
+//);
 
 //$.ajax({
 //    url: '/Catalogus/ReserveerProducten',
 //    type: 'post',
 //    data: { items: items }
 //});
-$(".toggle").change(function () {
-    $(".toggle").addClass("btn-succes");
-    $(".toggle").removeClass("btn-error");
-}, function () {
-    $(".toggle").show();
-    $(".toggle").hide();
+
+alert("test1");
+$(document).ready(function () {
+    alert("test1");
+    //event.preventDefault();
+    // Connect to "change" event in order to toggle glyphs
+    if ($(".toggle").prop("checked")) {
+        alert("test2");
+        $(this).prev().addClass("glyphicon-ok-circle");
+        $(this).prev().removeClass("glyphicon-unchecked");
+     //   $(this).prev().prev().addClass("btn-danger");
+     //   $(this).prev().prev().removeClass("groen");
+    } else {
+        $(this).prev().removeClass("glyphicon-ok-circle");
+        $(this).prev().addClass("glyphicon-unchecked");
+      // $(this).prev().prev().addClass("groen");
+       //$(this).prev().prev().removeClass("btn-danger");
+    }
+    /*$('.toggle').click(function (e) {
+        if ($(e.target).is('input')) { // prevent double-event due to bubbling
+            $(this).find('.glyphicon').toggleClass('glyphicon-check glyphicon-unchecked');
+        }
+    });*/
 });
