8bf2f1229bb0f3ccfe0955a47a36e3d52cf25ba1
Thomas Vanderschaeve <thomas.vanderschaeve.v5230@student.hogent.be>
2016-03-20 12:48:00 +0100

    Layout: Knop veranderd van catalogus voor verlanglijst!!!

--- Groep9.NET/Scripts/custom.js
+++ Groep9.NET/Scripts/custom.js
@@ -16,18 +16,7 @@ function mobileViewUpdate() {
 $(window).load(mobileViewUpdate);
 $(window).resize(mobileViewUpdate);
 
-//var items = $.map(
-//    $(this).sortable('toArray'),
-//    function (item, index) {
-//        return { "item": item };
-//    }
-//);
 
-//$.ajax({
-//    url: '/Catalogus/ReserveerProducten',
-//    type: 'post',
-//    data: { items: items }
-//});
 $('#sandbox-container .input-group.date').datepicker({
     format: "dd/mm/yyyy",
     language: "nl-BE",
@@ -35,24 +24,3 @@ $('#sandbox-container .input-group.date').datepicker({
     todayHighlight: true
 });
 
-$(document).ready(function () {
-    //event.preventDefault();
-    // Connect to "change" event in order to toggle glyphs
-    if ($(".toggle").prop("checked")) {
-        $(this).prev(".verlanglijstGlyphicon").addClass("glyphicon-ok-circle");
-        $(this).prev(".verlanglijstGlyphicon").removeClass("glyphicon-unchecked");
-        $(this).parent("label").addClass("btn-danger");
-        $(this).parent("label").removeClass("groen");
-        $(this).css("color:yellow;");
-    } else {
-        $(this).prev(".verlanglijstGlyphicon").removeClass("glyphicon-ok-circle");
-        $(this).prev(".verlanglijstGlyphicon").addClass("glyphicon-unchecked");
-        $(this).parent().addClass("groen");
-        $(this).parent().removeClass("btn-danger");
-    }
-    /*$('.toggle').click(function (e) {
-        if ($(e.target).is('input')) { // prevent double-event due to bubbling
-            $(this).find('.glyphicon').toggleClass('glyphicon-check glyphicon-unchecked');
-        }
-    });*/
-});
