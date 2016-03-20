b3f7e59824169ed258aa5b9e75b3e5684f38642b
Tim Van Dorpe <tim.van.dorpe@hotmail.com>
2016-03-20 17:17:27 +0100

    DomeinTest

--- Groep9.NET.Tests/Models/Domein/BlokkeringTest.cs
+++ Groep9.NET.Tests/Models/Domein/BlokkeringTest.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using Groep9.NET.Models.Domein;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Groep9.NET.Tests.Models.Domein {
     [TestClass]
     public class BlokkeringTest {
+
+        [ExpectedException(typeof(ArgumentException))]
         [TestMethod]
-        public void TestMethod1() {
+        public void TestBlokkeringConstructorArgumentExeption() {
+            int aantal = 1;
+            Product product = new Product();
+            Gebruiker student = new Student();
+            DateTime datum = DateTime.Now;
+            Blokkering x = new Blokkering(product, aantal, student, datum);
+
         }
     }
 }
